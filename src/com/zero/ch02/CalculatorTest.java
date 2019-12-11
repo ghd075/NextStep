75bc0eae0c75727eb32bb8185a0a0ea18368c36c
ghd075 <rlfls54@gmail.com>
2019-12-12 01:59:58 +0900

    Junit3 사용법!!

--- src/com/zero/ch02/CalculatorTest.java
+++ src/com/zero/ch02/CalculatorTest.java
@@ -8,20 +8,29 @@ public class CalculatorTest extends TestCase {
 	@Override
 	protected void setUp() throws Exception {
 		cal = new Calculator();
+		System.out.println("stetUp");
 	}
 	
 	public void testAdd() {
+		System.out.println("testAdd");
 		int result = cal.add(2, 1);
 		assertEquals(3, result);
 	}
 	
 	public void testSub() throws Exception {
+		System.out.println("testSub");
 		int result = cal.subtract(2, 1);
 		assertEquals(1, result);
 	}
 	
 	public void testMul() throws Exception {
+		System.out.println("testMul");
 		int result = cal.multiply(3, 2);
 		assertEquals(6, result);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		System.out.println("tearDown");
 	}
 }
